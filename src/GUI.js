c279ea78eb41faa27a9934c25a7138640f738fb3
kienkuber <kien.141315@gmail.com>
2018-07-22 20:21:00 +0700

    Kien&Tho fix bug o fullscreen button va stretch het man hinh khi o fullscreen mode

--- src/GUI.js
+++ src/GUI.js
@@ -56,12 +56,34 @@ var container,
 
 function initFullscreen() {
   $(".h3r-fullscreen").click(function () {
-    var el = container[0];
-    if (el.webkitRequestFullScreen) {
-      el.webkitRequestFullScreen();
-    } else {
-      el.mozRequestFullScreen();
-    }
+    var elem = container[0] || document.documentElement;
+      if (!document.fullscreenElement && !document.mozFullScreenElement &&
+          !document.webkitFullscreenElement && !document.msFullscreenElement) {
+          if (elem.requestFullscreen) {
+              elem.requestFullscreen();
+          } else if (elem.msRequestFullscreen) {
+              elem.msRequestFullscreen();
+          } else if (elem.mozRequestFullScreen) {
+              elem.mozRequestFullScreen();
+          } else if (elem.webkitRequestFullscreen) {
+              elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+          }
+      } else {
+          if (document.exitFullscreen) {
+              document.exitFullscreen();
+          } else if (document.msExitFullscreen) {
+              document.msExitFullscreen();
+          } else if (document.mozCancelFullScreen) {
+              document.mozCancelFullScreen();
+          } else if (document.webkitExitFullscreen) {
+              document.webkitExitFullscreen();
+          }
+      }
+    // if (el.webkitRequestFullScreen) {
+    //   el.webkitRequestFullScreen();
+    // } else {
+    //   el.mozRequestFullScreen();
+    // }
   });
 }
 
